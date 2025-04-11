a22061cc33fb0947946070c03ed433427e4733ae
Samy <mohamed3573579@gmail.com>
2025-04-11 20:40:23 +0200

    Your commit message here

--- dataBase/dbConnection.js
+++ dataBase/dbConnection.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const conn = mongoose.connect(`mongodb+srv://samy:${process.env.DB_PASS}@cluster0.irwnqhb.mongodb.net/shaboot`)
+export const conn = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irwnqhb.mongodb.net/${process.env.DB_NAME}`)
 .then(()=>{
     console.log('dataBase connected successfuly...')
 }).catch((error)=>{
